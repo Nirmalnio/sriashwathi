43277a8a252c19bd947914ff14498d5f324fa85d
Nirmal <nirmalnio123@gmail.com>
2023-11-05 01:38:32 +0530

    issues fixex

--- src/page/index.jsx
+++ src/page/index.jsx
@@ -4,20 +4,24 @@ import Header from "../components/layout/Header";
 import HomeprojectSection from "../components/homepage/HomeprojectSection";
 import HomeMidSection from "../components/homepage/HomeMidSection";
 import Footer from "../components/layout/Footer";
-
+import imge from "../assests/images/2 (1).png"
 function Home() {
-  
   useEffect(() => {
-    window.scrollTo(0,0)
+    window.scrollTo(0, 0);
   }, []);
 
   return (
-    <div>
-      <Header />
-      <HomeTopSection />
-      <HomeprojectSection />
-      <HomeMidSection />
-      <Footer />
+    <div className="ashu-main-container">
+      <div className="ashu-components">
+        <Header />
+        <HomeTopSection />
+        <HomeprojectSection />
+        <HomeMidSection />
+        <Footer />
+      </div>
+      <div className="mob-img-ashu">
+        <img src={imge} alt="mobile img" />
+      </div>
     </div>
   );
 }
